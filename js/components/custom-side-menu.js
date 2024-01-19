6c1f8947332fff1a57b4abec58caaa5dc35c9cba
Mood__Panda <andre-enju@ukr.net>
2024-01-19 23:40:47 +0200

    other side menu

--- js/components/custom-side-menu.js
+++ js/components/custom-side-menu.js
@@ -7,14 +7,47 @@ const sideMenuButtonClose = document.querySelector(".side-menu-close");
 const openSideMenu = () => {
     sideMenu.classList.add("side-menu-active");
     showOverlay();
-}
+};
 
 const closeSideMenu = () => {
     sideMenu.classList.remove("side-menu-active");
     hideOverlay();
-}
-
+};
 
 overlay.addEventListener("click", closeSideMenu);
 sideMenuButtonOpen.addEventListener("click", openSideMenu);
 sideMenuButtonClose.addEventListener("click", closeSideMenu);
+
+const amazonMusic = document.getElementById("amazon-music");
+const sideMenuOther = document.querySelector(".side-menu-other");
+const goBackMainMenu = document.querySelector(".go-back-main-menu");
+const sideMenuMain = document.querySelector(".side-menu-main");
+console.log(goBackMainMenu);
+
+const openSideMenuOther = () => {
+    sideMenuOther.classList.add("side-menu-other-active");
+    sideMenuMain.classList.add("side-menu-main-hide");
+};
+
+const closeSideMenuOther = () => {
+    sideMenuOther.classList.remove("side-menu-other-active");
+    sideMenuMain.classList.remove("side-menu-main-hide");
+};
+
+const handleClickSideMenu = (event) => {
+    const isAmazonMusic = event.target === amazonMusic;
+    if (isAmazonMusic) {
+        if (!amazonMusic.classList.contains("side-menu-other-active")) {
+            openSideMenuOther();
+        }
+    }
+
+    const isGoBackMainMenu = event.target === goBackMainMenu;
+    console.log(isGoBackMainMenu);
+    if (isGoBackMainMenu) {
+        closeSideMenuOther();
+    }
+};
+
+amazonMusic.addEventListener("click", handleClickSideMenu);
+goBackMainMenu.addEventListener("click", handleClickSideMenu);
