e571a03f5b5098525cff6d44cc2ba47abebb5c5f
Mood__Panda <andre-enju@ukr.net>
2024-01-25 17:22:38 +0200

    custom change language

--- js/components/custom-language-change.js
+++ js/components/custom-language-change.js
@@ -1,25 +1,66 @@
 const ukrainianCheckbox = document.getElementById("ukrainianCheckbox");
-const labelUkrainianCheckbox = document.querySelector("label[for='ukrainianCheckbox']");
+const labelUkrainianCheckbox = document.querySelector(
+    "label[for='ukrainianCheckbox']"
+);
+
+const englishCheckbox = document.getElementById("englishCheckbox");
+const labelEnglishCheckbox = document.querySelector(
+    "label[for='englishCheckbox']"
+);
 
 async function loadLanguageTexts(language) {
     const response = await fetch(`language-change-json/texts-${language}.json`);
     const data = await response.json();
-    console.log(data);
-    console.log(response);
     return data;
 }
 
-async function changeLanguage() {
-    const selectedLanguage = ukrainianCheckbox.checked ? "uk" : "en";
-    console.log(selectedLanguage);
-
-    const languageTexts = await loadLanguageTexts(selectedLanguage);
-    console.log(languageTexts);
-
+async function changeLanguage(language) {
+    const languageTexts = await loadLanguageTexts(language);
     document.querySelector(".deliver").textContent = languageTexts.deliver;
     document.querySelector(".region").textContent = languageTexts.region;
+    document.querySelector(".selected").textContent = languageTexts.selected;
 }
 
-ukrainianCheckbox.addEventListener("change", changeLanguage);
+englishCheckbox.addEventListener("change", async function () {
+    if (this.checked) {
+        saveSelectedLanguage("en");
+        await changeLanguage("en");
+        labelEnglishCheckbox.parentElement.classList.add("active-checkbox");
+        labelUkrainianCheckbox.parentElement.classList.remove(
+            "active-checkbox"
+        );
+        ukrainianCheckbox.checked = false; // Знімаємо вибір з іншого чекбоксу
+    }
+});
 
-changeLanguage();
+ukrainianCheckbox.addEventListener("change", async function () {
+    if (this.checked) {
+        saveSelectedLanguage("uk");
+        await changeLanguage("uk");
+        labelUkrainianCheckbox.parentElement.classList.add("active-checkbox");
+        labelEnglishCheckbox.parentElement.classList.remove("active-checkbox");
+        englishCheckbox.checked = false; // Знімаємо вибір з іншого чекбоксу
+    }
+});
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const selectedLanguage = loadSelectedLanguage();
+
+    if (selectedLanguage === "uk") {
+        ukrainianCheckbox.checked = true;
+        labelUkrainianCheckbox.parentElement.classList.add("active-checkbox");
+        await changeLanguage("uk");
+    } else {
+        englishCheckbox.checked = true;
+        labelEnglishCheckbox.parentElement.classList.add("active-checkbox");
+        await changeLanguage("en");
+    }
+});
+
+function saveSelectedLanguage(language) {
+    localStorage.setItem("selectedLanguage", language);
+}
+
+function loadSelectedLanguage() {
+    return localStorage.getItem("selectedLanguage") || "en";
+}
---
+++ none.js
@@ -0,0 +1,117 @@
+const ukrainianCheckbox = document.getElementById("ukrainianCheckbox");
+const labelUkrainianCheckbox = document.querySelector(
+    "label[for='ukrainianCheckbox']"
+);
+
+const englishCheckbox = document.getElementById("englishCheckbox");
+const labelEnglishCheckbox = document.querySelector(
+    "label[for='englishCheckbox']"
+);
+
+// тут буду отримувати аснхронно доступ до файлів json де зберігаються переклади мов
+async function loadLanguageTexts(language) {
+    const response = await fetch(`language-change-json/texts-${language}.json`);
+    const data = await response.json();
+    return data;
+}
+
+async function changeLanguage() {
+    // Отримуємо обрану англійську мову, якщо жоден інший чекбокс не вибрано
+    const selectedLanguage = ukrainianCheckbox.checked ? "uk" : "en";
+
+    saveSelectedLanguage(selectedLanguage);
+
+    // Обираємо мову залежно від вибору користувача
+    const languageTexts = await loadLanguageTexts(selectedLanguage);
+    document.querySelector(".deliver").textContent = languageTexts.deliver;
+    document.querySelector(".region").textContent = languageTexts.region;
+}
+
+englishCheckbox.addEventListener("change", changeLanguage);
+ukrainianCheckbox.addEventListener("change", changeLanguage);
+
+// Зберігаємо обрану мову в локальному сховищі
+function saveSelectedLanguage(language) {
+    localStorage.setItem("selectedLanguage", language);
+}
+
+// Завантажуємо обрану мову з локального сховища
+function loadSelectedLanguage() {
+    return localStorage.getItem("selectedLanguage") || "en";
+}
+
+const languageCheckboxes = document.querySelectorAll(".checkbox");
+
+document.addEventListener("DOMContentLoaded", async function () {
+    // Завантажуємо обрану мову з локального сховища
+    const selectedLanguage = loadSelectedLanguage();
+
+    // Встановлюємо обрану мову відповідно до локального сховища
+    if (selectedLanguage === "uk") {
+        ukrainianCheckbox.checked = true;
+        labelUkrainianCheckbox.parentElement.classList.add("active-checkbox");
+    } else {
+        englishCheckbox.checked = true;
+        labelEnglishCheckbox.parentElement.classList.add("active-checkbox");
+    }
+
+    await changeLanguage();
+
+    // Встановлюємо подію зміни мови для кожного чекбокса
+    [ukrainianCheckbox, englishCheckbox].forEach((checkbox) => {
+        checkbox.addEventListener("change", async function () {
+            // Змінюємо значення мови в локальному сховищі
+            saveSelectedLanguage(
+                checkbox.id === "ukrainianCheckbox" ? "uk" : "en"
+            );
+
+            // Знімаємо попередній вибір з усіх чекбоксів
+            [ukrainianCheckbox, englishCheckbox].forEach((cb) => {
+                if (cb !== checkbox) {
+                    cb.checked = false;
+                    cb.parentElement.classList.remove("active-checkbox");
+                }
+            });
+
+            // Позначаємо обраний чекбокс як активний
+            if (checkbox.checked) {
+                // checkbox.checked = true;
+                checkbox.parentElement.classList.add("active-checkbox");
+                await changeLanguage();
+            }
+
+            // Змінюємо мову
+            await changeLanguage();
+        });
+    });
+
+    // Викликаємо зміну мови при завантаженні сторінки
+    await changeLanguage();
+
+    // Перевіряємо, чи чекбокс для англійської мови не встановлений
+    if (!englishCheckbox.checked) {
+        // Якщо чекбокс не встановлений, встановлюємо за замовчуванням англійську мову
+        englishCheckbox.checked = true;
+        labelEnglishCheckbox.parentElement.classList.add("active-checkbox");
+    }
+
+    // Встановлюємо подію зміни мови для кожного чекбокса
+    languageCheckboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", function () {
+            // Знімаємо попередній вибір з усіх чекбоксів
+            languageCheckboxes.forEach((cb) => {
+                if (cb !== this) {
+                    cb.checked = false;
+                    cb.parentElement.classList.remove("active-checkbox");
+                }
+            });
+
+            // Позначаємо обраний чекбокс як активний
+            if (this.checked) {
+                this.parentElement.classList.add("active-checkbox");
+            }
+
+            changeLanguage();
+        });
+    });
+});
